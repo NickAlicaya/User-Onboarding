87774c1d1b743170891ab5c023e9e8a18b4ea3d9
nicanor-alicaya <donalicaya@gmail.com>
2019-11-15 00:07:08 -0500

    Added reset on submit and validationSchema for terms and role dropdown options

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import NewForm from'./components/NewForm';
 import './App.css';
+// import { useFormik } from 'formik';
 
 function App() {
   return (
     <div className="App">
-     <NewForm />
+     <NewForm/>
     </div>
   );
 }
--- user-onboarding/src/components/NewForm.js
+++ user-onboarding/src/components/NewForm.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from'react';
 import { withFormik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 import axios from'axios';
+import styled from'styled-components';
+// import { Button, Form as RForm, FormGroup, Label, Input, FormText } from 'reactstrap';
+
 
 const NewForm = ({ values, errors, touched, status }) => {
     const [ users, setUsers ] =useState([]);
@@ -11,9 +14,12 @@ const NewForm = ({ values, errors, touched, status }) => {
     }, [status]);
 
     return(
-        <div className='newForm'>
+        <Formdesign className='newForm'>
+            
             <Form>
-                <div>
+                <h1>Register</h1>
+                <Divblocks>
+                {/* <Label>Name:</Label> */}
                 <Field type='text'
                 name='name'
                 placeholder='Enter Full Name'
@@ -21,9 +27,11 @@ const NewForm = ({ values, errors, touched, status }) => {
                 {touched.name && errors.name && (
                     <p className='errors'>{errors.name}</p>
                 )}
-                </div>
+                </Divblocks>
 
-                <div>       
+                
+                <Divblocks>   
+                {/* <Label>Email:</Label>         */}
                 <Field type='email'
                 name='email'
                 placeholder='Enter Email'
@@ -31,9 +39,11 @@ const NewForm = ({ values, errors, touched, status }) => {
                 {touched.email && errors.email && (
                     <p className='errors'>{errors.email}</p>
                 )}
-                </div> 
+                </Divblocks> 
 
+                       
                 <div> 
+                {/* <Label>Password:</Label>      */}
                 <Field type='text'
                 name='password'
                 placeholder='Enter Password'
@@ -52,9 +62,9 @@ const NewForm = ({ values, errors, touched, status }) => {
                 <option value='designer'>Designer</option>
                 <option value='teamleader'>Team Leader</option>
                 </Field>
-                {/* {touched.role && errors.role && (
+                {touched.role && errors.role && (
                     <p className='errors'>{errors.role}</p>
-                )} */}
+                )}
                 </div> 
                 
                 <div>         
@@ -65,19 +75,24 @@ const NewForm = ({ values, errors, touched, status }) => {
                     name='terms'
                     checked={values.terms}
                     />
+                    {touched.terms && errors.terms && (
+                    <p className='errors'>{errors.terms}</p>
+                )}
                 </label>  
                 </div> 
 
                 <div> <button type='submit'>Submit</button></div> 
             </Form>
             {users.map(user =>(
-                <div key={user.name}>
+                <div>
+                <Usercard key={user.id}>
                     <h3>Name: {user.name}</h3>
                     <div>Email: {user.email}</div>
                     <div>Role: {user.role}</div>
+               </Usercard>
                </div>
             ))}
-        </div>
+        </Formdesign>
     );
 };
 
@@ -94,7 +109,9 @@ const FormikNewForm = withFormik({
 validationSchema: Yup.object().shape({
     name: Yup.string().required(),
     email: Yup.string().required(),
-    password: Yup.string().required()
+    password: Yup.string().required(),
+    role: Yup.string().notOneOf(["Please Choose a role"]).required("Please select one"),
+    terms: Yup.boolean().required().oneOf([true], "Must agree to terms and conditions") 
     }),
 handleSubmit( values, {setStatus, resetForm} ) {
 
@@ -103,11 +120,27 @@ handleSubmit( values, {setStatus, resetForm} ) {
         .then(res => {
             setStatus(res.data);
             console.log('RESPONSE',res);
+            resetForm()
         })
         .catch(err => console.log(err.response));
     }
 
 })(NewForm);
+
+const Formdesign = styled.div`
+width:30%;
+margin: auto;
+background-color:rgb(214,216,217);
+`;
+
+const Divblocks = styled.div`
+margin: 10px auto,
+`;
+
+const Usercard = styled.div`
+border-1px solid red;
+`;
+
 export default FormikNewForm
 console.log('This is the HOC', FormikNewForm)
 
